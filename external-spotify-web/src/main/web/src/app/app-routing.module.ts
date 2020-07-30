8d2453ea17cb588ef24042a3a2f99823b612ae8f
hajdumozes <hajdu.mozes@gmail.com>
2020-07-30 20:12:24 +0200

    removed root route #1

--- external-spotify-web/src/main/web/src/app/app-routing.module.ts
+++ external-spotify-web/src/main/web/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SpotifyUriCallbackComponent } from './spotify-uri-callback/spotify-uri-callback.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
   {
     path: 'spotify-uri-callback',
     component: SpotifyUriCallbackComponent,
