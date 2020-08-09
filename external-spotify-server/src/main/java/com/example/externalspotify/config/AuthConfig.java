06afc8c274fb85cf5b8108a0c1ece3e710c84bbc
hajdumozes <hajdu.mozes@gmail.com>
2020-08-09 15:00:06 +0200

    default url is 4200 #1

--- external-spotify-server/src/main/java/com/example/externalspotify/config/AuthConfig.java
+++ external-spotify-server/src/main/java/com/example/externalspotify/config/AuthConfig.java
@@ -11,5 +11,5 @@ public class AuthConfig {
     @Getter
     private static final String clientSecret = "0bdd3a319c794da1a4363a1c0c8db6b6";
     @Getter
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/spotify-uri-callback");
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:4200/spotify-uri-callback");
 }
