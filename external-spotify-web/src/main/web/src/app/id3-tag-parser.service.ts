3cec03b3545c9aff6d9fbc0c64ec31d39b95f17e
hajdumozes <hajdu.mozes@gmail.com>
2020-08-11 19:56:57 +0200

    parse service refactor to async/await #4

--- external-spotify-web/src/main/web/src/app/id3-tag-parser.service.ts
+++ external-spotify-web/src/main/web/src/app/id3-tag-parser.service.ts
@@ -16,48 +16,44 @@ const debug = createDebug('audio-tag-analyzer:id3-tag-parser-service');
 export class Id3TagParserService {
   constructor(private zone: NgZone) {}
 
-  public parseUsingHttp(url: string): Promise<mm.IAudioMetadata> {
-    debug('Converting HTTP to stream using: ' + url);
+  public async parseUsingHttp(url: string): Promise<mm.IAudioMetadata> {
+    try {
+      debug('Converting HTTP to stream using: ' + url);
 
-    const file: IUrlAsFile = {
-      name: url,
-      type: '?',
-    };
-
-    return mm
-      .fetchFromUrl(url)
-      .then((metadata) => {
-        this.zone.run(() => {
-          debug('Completed parsing of %s:', file.name, metadata);
-          return metadata;
-        });
-      })
-      .catch((err) => {
-        this.zone.run<void>(() => {
-          debug('Error: ' + err.message);
-        });
-      }) as any;
+      const file: IUrlAsFile = {
+        name: url,
+        type: '?',
+      };
+      const metadata: mm.IAudioMetadata = await mm.fetchFromUrl(url);
+      this.zone.run(() => {
+        debug('Completed parsing of %s:', file.name, metadata);
+      });
+      return metadata;
+    } catch (err) {
+      this.zone.run<void>(() => {
+        debug('Error: ' + err.message);
+      });
+    }
   }
 
-  public parseFile(file: File): Promise<mm.IAudioMetadata> {
-    const t0 = new Date().getTime();
-    debug('Parsing %s of type %s', file.name, file.type);
+  public async parseFile(file: File): Promise<mm.IAudioMetadata> {
+    try {
+      const t0 = new Date().getTime();
+      debug('Parsing %s of type %s', file.name, file.type);
 
-    return mm
-      .parseBlob(file)
-      .then((metadata) => {
-        const t1 = new Date().getTime();
-        const duration = t1 - t0;
-        debug(`Parsing took ${duration} ms`);
-        this.zone.run(() => {
-          debug('Completed parsing of %s:', file.name, metadata);
-        });
-        return metadata;
-      })
-      .catch((err) => {
-        this.zone.run<void>(() => {
-          debug('Error: ' + err.message);
-        });
-      }) as any;
+      const metadata: mm.IAudioMetadata = await mm.parseBlob(file);
+
+      const t1 = new Date().getTime();
+      const duration = t1 - t0;
+      debug(`Parsing took ${duration} ms`);
+      this.zone.run(() => {
+        debug('Completed parsing of %s:', file.name, metadata);
+      });
+      return metadata;
+    } catch (err) {
+      this.zone.run<void>(() => {
+        debug('Error: ' + err.message);
+      });
+    }
   }
 }
