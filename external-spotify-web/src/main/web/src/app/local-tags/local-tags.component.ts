2649f81afdb219d481daacdea561ca1e2dd874f3
hajdumozes <hajdu.mozes@gmail.com>
2020-08-12 20:19:26 +0200

    exact matches html addition #5

--- external-spotify-web/src/main/web/src/app/local-tags/local-tags.component.ts
+++ external-spotify-web/src/main/web/src/app/local-tags/local-tags.component.ts
@@ -13,7 +13,7 @@ const debug = createDebug('audio-tag-analyzer:local-tags-component');
   styleUrls: ['./local-tags.component.css'],
 })
 export class LocalTagsComponent implements OnInit {
-  public results: SpotifyTrack[];
+  public exactMatches: SpotifyTrack[];
 
   constructor(
     private id3TagParserService: Id3TagParserService,
@@ -23,23 +23,23 @@ export class LocalTagsComponent implements OnInit {
   ngOnInit(): void {}
 
   public async handleFilesDropped(files: File[]) {
-    this.results = []; // initialize results
+    this.exactMatches = []; // initialize results
     debug('handleFilesDropped', files);
     for (const file of files) {
       debug('Start parsing file %s', file.name);
       const tag = await this.id3TagParserService.parseFile(file);
       this.spotifyService.getTrackFromSpotify(tag).subscribe((tracks) => {
-        tracks.forEach((track) => this.results.push(track));
+        tracks.forEach((track) => this.exactMatches.push(track));
       });
     }
   }
 
   public async handleTextDropped(text) {
-    this.results = []; // initialize results
+    this.exactMatches = []; // initialize results
     if (text.indexOf('http') === 0) {
       const tag = await this.id3TagParserService.parseUsingHttp(text);
       this.spotifyService.getTrackFromSpotify(tag).subscribe((tracks) => {
-        tracks.forEach((track) => this.results.push(track));
+        tracks.forEach((track) => this.exactMatches.push(track));
       });
     } else {
     }
