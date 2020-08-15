a29feb8af6bd956d533eea7b3049d0a864c7b33d
hajdumozes <hajdu.mozes@gmail.com>
2020-08-15 19:18:11 +0200

    util contains #11

--- external-spotify-web/src/main/web/src/app/util/Utils.ts
+++ external-spotify-web/src/main/web/src/app/util/Utils.ts
@@ -1,9 +1,15 @@
+import * as deepEqual from 'deep-equal';
+
 export default class Utils {
-  static chunkArray(array, chunkSize) {
+  static chunkArray(array: any[], chunkSize: number): any[][] {
     let results = [];
     while (array.length) {
       results.push(array.splice(0, chunkSize));
     }
     return results;
+  }
+
+  static findIndexOf(array: any[], object: any): number {
+    return array.findIndex((item) => deepEqual(item, object));
   }
 }
