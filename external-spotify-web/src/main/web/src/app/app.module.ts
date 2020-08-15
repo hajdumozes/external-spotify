2ee6c1dbef77440958fe1afc5a38478d1cd6f365
hajdumozes <hajdu.mozes@gmail.com>
2020-08-15 16:25:03 +0200

    angular material addition #12

--- external-spotify-web/src/main/web/src/app/app.module.ts
+++ external-spotify-web/src/main/web/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { SpotifyUriCallbackComponent } from './spotify-uri-callback/spotify-uri-callback.component';
 import { LocalTagsComponent } from './local-tags/local-tags.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
